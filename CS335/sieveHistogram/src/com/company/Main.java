4ea7836431f1ab393020acf6149b8e94d301e01a
cpazzra <nicky.shadow99@gmail.com>
2022-01-19 14:38:56 -0500

    histogram can be assigned an interval

--- CS335/sieveHistogram/src/com/company/Main.java
+++ CS335/sieveHistogram/src/com/company/Main.java
@@ -7,16 +7,16 @@ public class Main {
 
     public static void main(String[] args) {
         // Sieve of one million elements
-        PrimeSieve sieve = new PrimeSieve(1000);
+        // Will take a while. Run with smaller size to get the same idea.
+        PrimeSieve sieve = new PrimeSieve(100000);
 
         sieve.run();
 
         // Draw histogram
-        sieve.histogram();
+        sieve.histogram(10);
     }
 
 }
-
 
 class PrimeSieve {
 
@@ -43,23 +43,25 @@ class PrimeSieve {
         }
     }
 
-    void histogram() {
+    void histogram(int numberOfIntervals) {
         System.out.println("The CS335 Prime Number Histogram (# of Primes in each interval)");
 
         // Use collections to cleanly count the number of primes
         List<Boolean> primeList = Arrays.stream(isPrime).collect(Collectors.toList());
 
-        printHistogram(primeList);
+        printHistogram(numberOfIntervals, primeList);
     }
 
     /**
      * Given a list of booleans, print out the number of prime numbers in intervals of 100
      */
-    private void printHistogram(List<Boolean> primeList) {
+    private void printHistogram(int numberOfIntervals, List<Boolean> primeList) {
         // Set initial interval
+        int intervalSize = primeList.size() / numberOfIntervals;
         int startOfInterval = 0;
-        int endOfInterval = 100;
+        int endOfInterval = intervalSize;
         int intervalPrimeCount = 0;
+
         for (int currentInterval = 0; currentInterval < primeList.size(); currentInterval++) {
             if (Objects.nonNull(primeList.get(currentInterval)) && primeList.get(currentInterval)) {
                 intervalPrimeCount++;
@@ -69,8 +71,8 @@ class PrimeSieve {
                 // Print out current interval information
                 System.out.println("Interval " + startOfInterval + " - " + currentInterval + " count: " + intervalPrimeCount);
                 // Reset for next interval
-                startOfInterval += 100;
-                endOfInterval += 100;
+                startOfInterval += intervalSize;
+                endOfInterval += intervalSize;
                 if (endOfInterval >= primeList.size()) {
                     endOfInterval = primeList.size();
                 }
