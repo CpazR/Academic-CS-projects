8a53b80b1cd1f24b8109ce828f38343608e42e98
Cpaz <nicky.shadow99@gmail.com>
2022-02-05 17:05:32 -0500

    cardmaching game base
    based on shuffler

---
+++ CS335/cardmatch/src/com/company/Card.java
@@ -0,0 +1,91 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Card {
+
+    enum Suit {
+        SPADES(0, "spades", "black"),
+        HEARTS(1, "hearts", "red"),
+        DIAMONDS(2, "diamonds", "red"),
+        CLUBS(3, "clubs", "black");
+
+        private final String name;
+
+        Suit(int suitNumber, String suitName, String color) {
+            name = suitName;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    enum Value {
+        A(1, "ace"),
+        TWO(2, "2"),
+        THREE(3, "3"),
+        FOUR(4, "4"),
+        FIVE(5, "5"),
+        SIX(6, "6"),
+        SEVEN(7, "7"),
+        EIGHT(8, "8"),
+        NINE(9, "9"),
+        TEN(10, "10"),
+        JACK(11, "jack"),
+        QUEEN(12, "queen"),
+        KING(13, "king");
+
+        private final String name;
+
+        Value(int valueIndex, String valueName) {
+            name = valueName;
+        }
+
+        /**
+         * Get enum from an index from 1 - 13
+         */
+        static Value valueOf(int index) throws Exception {
+            if (index < 1 || index > 13) {
+                throw new Exception("Card number is supposed to be between 1 and 13 but was: " + index);
+            }
+            return values()[index - 1];
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    /**
+     * The numerical value of the card.
+     * <br> 1 = A </br>
+     * <br> 2 - 10 = Normal values </br>
+     * <br> 11 = Jack </br>
+     * <br> 12 = Queen </br>
+     * <br> 13 = King </br>
+     */
+    private final Value value;
+    private final Suit suit;
+    private final Icon cardImage;
+
+    private final Icon notFlippedImage = new ImageIcon("./assets/red_joker_icon.png");
+    private boolean flippedOver = false;
+
+    Card(Suit suit, Value number) {
+        this.suit = suit;
+        this.value = number;
+        this.cardImage = new ImageIcon("./assets/" + this.value + "_of_" + this.suit + "_icon.png");
+    }
+
+    public Icon getCardImage() {
+        return cardImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Suit: " + suit.name() + " Value: " + value;
+    }
+}
