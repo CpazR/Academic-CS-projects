c602c5a538f0293f1fff230508e3c6c7ac31122d
cpazzra <nicky.shadow99@gmail.com>
2022-02-12 17:24:19 -0500

    networking project base
    kinda broken atm

--- CS371/Socket-Server/src/com/company/Main.java
+++ CS371/Socket-Server/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,42 +10,74 @@ import java.net.Socket;
 public class Main {
 
     public static void main(String[] args) {
-        final int serverPort = 80;
+        final int serverPort = 4444;
         Socket socket = null;
         ServerSocket serverSocket = null;
         DataInputStream inputStream = null;
         DataOutputStream outputStream = null;
 
-        try {
-            // Establish server on port {#serverPort}
-            serverSocket = new ServerSocket(serverPort);
-            System.out.println("Server started");
+        boolean lookForClients = true;
 
-            // Wait for client to connect
-            System.out.println("Waiting for client...");
-            socket = serverSocket.accept();
+        while (lookForClients) {
+            try {
+                // Establish server on port {#serverPort}
+                serverSocket = new ServerSocket(serverPort);
+                System.out.println("Server started");
 
-            // Client connected, establish data input/output
-            System.out.println("Client accepted");
-            inputStream = new DataInputStream(socket.getInputStream());
-            outputStream = new DataOutputStream(System.out);
+                // Wait for client to connect
+                System.out.println("Waiting for client...");
+                socket = serverSocket.accept();
 
-            String inputFromClient = "";
+                // Client connected, establish data input/output
+                System.out.println("Client accepted");
+                inputStream = new DataInputStream(socket.getInputStream());
+                outputStream = new DataOutputStream(System.out);
 
-            while (!inputFromClient.equals("close")) {
-                try {
-                    inputFromClient = inputStream.readUTF();
-                    System.out.println(inputFromClient);
-                } catch (IOException i) {
-                    i.printStackTrace();
+                String inputFromClient = "";
+
+                while (!inputFromClient.equals("close")) {
+                    try {
+                        System.out.println("Waiting for client input...");
+                        inputFromClient = inputStream.readUTF();
+                        performAction(inputFromClient);
+                        System.out.println(inputFromClient);
+                    } catch (Exception i) {
+                        i.printStackTrace();
+                    }
                 }
-            }
+                System.out.println("Closing...");
 
-            // Close server
-            socket.close();
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+                // Close server
+                socket.close();
+                inputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void performAction(String inputFromClient) {
+        var directory = "./files/";
+        switch (inputFromClient) {
+            case "CREATE":
+                // Create a path local to the server
+                var file = new File(directory + "somefile.txt");
+                if (file.isDirectory()) {
+                    try {
+                        file.createNewFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    if (file.mkdir()) {
+                        try {
+                            file.createNewFile();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                break;
         }
     }
 }
--- CS371/SocketClient/src/com/company/Main.java
+++ CS371/SocketClient/src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,13 +14,30 @@ public class Main {
         Socket mainSocket = null;
         DataInputStream inputStream = null;
         DataOutputStream outputStream = null;
+        int clientPort = 4444;
 
         try {
-            mainSocket = new Socket("127.0.0.1", 80);
+            mainSocket = new Socket("127.0.0.1", clientPort);
             System.out.println("Client connected");
 
             inputStream = new DataInputStream(System.in);
             outputStream = new DataOutputStream(mainSocket.getOutputStream());
+            Scanner inputScanner = new Scanner(System.in);
+            inputScanner.useDelimiter("\\n");
+            String localUserInput = inputScanner.nextLine();
+
+            while (!localUserInput.equals("close")) {
+                try {
+                    outputStream.writeUTF(localUserInput);
+                    System.out.println("Sent: " + localUserInput + " to server");
+                } catch (UnknownHostException u) {
+                    u.printStackTrace();
+                }
+
+                // Get new input
+                localUserInput = inputScanner.nextLine();
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
