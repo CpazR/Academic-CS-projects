5e2e7e449cb3e2d2e70c13d33e10df32e2ed1f71
Nicholas Reel <nicky.shadow99@gmail.com>
2023-02-03 13:10:10 -0500

    I think entropy and info gain is working???

--- CS460G/DecisionTrees/main.py
+++ CS460G/DecisionTrees/main.py
@@ -13,6 +13,8 @@ syntheticDatasets: list = [pandas.read_csv(os.getcwd() + '/data/synthetic-1.csv'
                            pandas.read_csv(os.getcwd() + '/data/synthetic-3.csv'),
                            pandas.read_csv(os.getcwd() + '/data/synthetic-4.csv')]
 
+maxTreeDepth = 3
+
 
 class DecisionTree:
     class Node:
@@ -30,6 +32,13 @@ class DecisionTree:
         # Equidistant
         def discretize(self, column: list):
             thresholdRange = [min(column), max(column)]  # array of min and max values for thresholds
+            # In some range, define thresholds
+            # Decide how many values you want to express in range
+            # k values. Find thresholds so that distance between each thresh is equal.
+            # So: thresholdOffset = (upperRange - lowerRange) / k
+            # threshValues = list of size k - 1
+            # EX: k = 5. threshValues = [2, 4, 6, 8]
+
             # Default to 10 bins. Downsize depending on datatype.
 
             if thresholdRange[1] - thresholdRange[0] == 0:
@@ -46,48 +55,82 @@ class DecisionTree:
 
             bins.append(thresholdRange[1])
 
-            discretizeBin = pandas.cut(x=column, bins=bins)
-            print(discretizeBin)
-
-            # In some range, define thresholds
-            # Decide how many values you want to express in range
-            # k values. Find thresholds so that distance between each thresh is equal.
-            # So: thresholdOffset = (upperRange - lowerRange) / k
-            # threshValues = list of size k - 1
-            # EX: k = 5. threshValues = [2, 4, 6, 8]
+            return pandas.cut(x=column, bins=bins)
 
         # end discretize
 
-        def entropy(self, dataset: list):
+        def entropy(self, column: list):
             print('todo')
             # Sum of - (# of i examples / total examples) * log2(# of i examples / total examples)
             # if (# of i examples / total examples)tree is 50/50, then skip calculation and default to 1
 
-            dataQuantity = len(dataset)
+            dataQuantity = len(column)
+
+            # Calculate frequency of all values
+            dataFrequency = dict()
+
+            for dataIndex in column:
+                # Add to frequency dictionary if not yet seen
+                if dataIndex not in dataFrequency:
+                    dataFrequency[dataIndex] = 0
+                # Increment 1 to the frequency dictionary
+                dataFrequency[dataIndex] += 1
 
             entropyValue = 0
 
-            for i in range(dataQuantity):
-                print(dataset[i])
-                numberOfPossibilities = 0
-                probability = (numberOfPossibilities / dataQuantity)
-                entropyValue += -probability * math.log2(probability)
+            # Skip entropy calculation if data is homogenous
+            if len(dataFrequency) > 1 and self.level >= maxTreeDepth:
+                for key, value in dataFrequency.items():
+                    # Frequency of specific value (key)
+                    frequency = value / len(column)
+                    # If value does not appear, skip to next iteration
+                    if frequency == 0:
+                        continue
+                    log = math.log(frequency, len(dataFrequency))
+                    entropyValue -= log * frequency
 
-            return 0
+            return entropyValue
 
         # end entropy
 
-        def informationGain(self, pCurrent, pChilren: list):
-            # difference of parent entropy and the wighted average entropy of children
-            entropyPCurrent = self.entropy(pCurrent)
+        # Given a target column and a column to split on, run information gain
+        def informationGain(self, targetColumn: list, splitColumn: list):
+            # Calculate parent entropy
+            parentEntropy = self.entropy(targetColumn)
 
-            entropyChildren = 0
+            # Store the frequency of both positive and negative class labels
+            positiveDataFrequency = dict()
+            negativeDataFrequency = dict()
 
-            for child in pChilren:
-                currentEquiv: list = []  # set where current = child
-                entropyChildren += len(pCurrent) / len(currentEquiv) * self.entropy(currentEquiv)
+            for index, cell in enumerate(splitColumn):
+                if cell not in positiveDataFrequency:
+                    positiveDataFrequency[cell] = 0
+                if cell not in negativeDataFrequency:
+                    negativeDataFrequency[cell] = 0
 
-            return entropyPCurrent - entropyChildren
+                # This is assuming that the splitColumn is a numerical boolean flag
+                if targetColumn[index] == 1:
+                    positiveDataFrequency[cell] += 1
+                if targetColumn[index] == 0:
+                    negativeDataFrequency[cell] += 1
+
+            # Calculate the wighted average entropy of children
+            childrenEntropy = 0
+
+            # NOTE: Since both positive and negative frequencies share keys, we can iterate over one of the
+            # dictionaries and use them on both
+            for key in positiveDataFrequency.keys():
+                dataSubset = list()
+                if positiveDataFrequency[key] != 0:
+                    dataSubset = list(positiveDataFrequency.values())
+                if negativeDataFrequency[key] != 0:
+                    dataSubset = list(negativeDataFrequency.values())
+
+                childProbability = len(dataSubset) / len(splitColumn)
+                childrenEntropy += childProbability * self.entropy(dataSubset)
+
+            #  Finish information gain calculation
+            return parentEntropy - childrenEntropy
 
         # end informationGain
 
